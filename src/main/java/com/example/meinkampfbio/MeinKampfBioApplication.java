85f9e4ef5a052e323657bd1fbea91b5eb1e12d50
MD. AREFUL ISLAM <32135229+arifislam007@users.noreply.github.com>
2024-09-23 22:44:58 +0600

    Create MeinKampfBioApplication.java

---
+++ src/main/java/com/example/meinkampfbio/MeinKampfBioApplication.java
@@ -0,0 +1,13 @@
+package com.example.meinkampfbio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeinKampfBioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeinKampfBioApplication.class, args);
+    }
+
+}
